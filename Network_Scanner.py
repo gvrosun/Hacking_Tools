1e049e731843241fb4d8e109d7b75a027d38b54d
root <gvrosun@gmail.com>
2020-07-06 03:32:50 -0400

    Network Scanner module ready!!!

--- Network_Scanner.py
+++ Network_Scanner.py
@@ -4,18 +4,24 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    # scapy.arping(ip)
-    arp_request = scapy.ARP(pdst=ip)   # Creating ARP packet with ip's
-    # print(arp_request.summary())
-    # print(scapy.ls(scapy.ARP()))  // Listing things we can set (options)
+    arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    # arp_request.show()  // To show details
-    # broadcast.show()    // To show details
-    # arp_request_broadcast.show()  # To show details
-    # answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)  # Sent packets and receive response or
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]  # we don't want unanswered list
-    print(answered_list.summary())
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    clients_list = []
+    for elements in answered_list:
+        clients_dict = {"ip": elements[1].psrc, "mac": elements[1].hwsrc}
+        clients_list.append(clients_dict)
+    return clients_list
 
 
-scan("10.0.2.1/24")
+def print_result(results_list):
+    print("IP\t\t\tMAC address")
+    print("-------------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+scan_result = scan("10.0.2.1/24")
+print_result(scan_result)
