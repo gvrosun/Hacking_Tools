32ccc666c6a544304fb8e36b0c287db866714711
root <gvrosun@gmail.com>
2020-07-25 11:59:41 +0530

    crawler.py v1.1.2 Beta

--- website/spider.py
+++ website/spider.py
@@ -4,16 +4,22 @@ import requests
 import re
 import urlparse
 
+target_url = "https://zsecurity.org"
+target_links = []
+
 
 def extract_links(url):
     response = requests.get(url)
     return re.findall('(?:href=")(.*?)"', response.content)
 
 
-target_url = "https://zsecurity.org"
 href_links = extract_links(target_url)
 for link in href_links:
     link = urlparse.urljoin(target_url, link)
 
-    if target_url in link:
+    if '#' in link:
+        link = link.split('#')[0]
+
+    if target_url in link and link not in target_links:
+        target_links.append(link)
         print(link)
